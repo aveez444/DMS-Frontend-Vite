8cc05ab0660dc4815fb62c483fa53d44a072c2fe
aveez444 <aveezsiddiqui656@gmail.com>
2025-07-18 12:08:49 +0530

    port

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: process.env.PORT || 4173,
+    port: process.env.PORT || 8080,
   },
 })
